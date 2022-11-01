55b7b9a202b6c30894b813e7ba9b67ea6d761a6a
KaraJH <karas.jones@yahoo.com>
2022-11-01 10:28:20 -0600

    TEsts for the election class object and variables

---
+++ spec/election_spec.rb
@@ -0,0 +1,13 @@
+require 'rspec'
+require './lib/candidate'
+require './lib/race'
+require './lib/election'
+
+RSpec.describe Election do
+  it 'is an eklection object with a year variable and races variable' do
+    election = Election.new('2022')
+
+    expect(election.year).to eq(2022)
+    expect(election.races).to eq([])
+  end
+end
