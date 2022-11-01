17057283310fbdc0f66b2d97ae87d96b9f87001f
KaraJH <karas.jones@yahoo.com>
2022-11-01 09:18:57 -0600

    Creates method for votes

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,8 +1,13 @@
 class Candidate
-  attr_reader :name, :party
+  attr_reader :name, :party, :num_of_votes
 
   def initialize(candidate_info)
     @name = candidate_info[:name]
     @party = candidate_info[:party]
+    @num_of_votes = []
+  end
+
+  def votes
+    num_of_votes.count
   end
 end
