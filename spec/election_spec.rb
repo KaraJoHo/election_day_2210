fa9f36aa76e96efb85964d8fe1bb9359a4a9c8a7
KaraJH <karas.jones@yahoo.com>
2022-11-01 10:29:51 -0600

    Fixes typo in test

--- spec/election_spec.rb
+++ spec/election_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Election do
   it 'is an eklection object with a year variable and races variable' do
     election = Election.new('2022')
 
-    expect(election.year).to eq(2022)
+    expect(election.year).to eq("2022")
     expect(election.races).to eq([])
   end
 end
