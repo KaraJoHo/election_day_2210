b813f40ad5e2940f55a8eb23c640f476827ae398
KaraJH <karas.jones@yahoo.com>
2022-11-01 09:36:12 -0600

    Tests for first part of the register_candidate method

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -17,4 +17,15 @@ RSpec.describe Race do
       expect(race.candidates).to eq([])
     end
   end
+
+  describe '#register_candidate!' do
+    it 'takes a has of a candidate object and returns the instance of Candidate' do
+      race = Race.new("Texas Governor")
+      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+
+      expect(candidate1).to be_a(Candidate)
+      expect(candidate1.name).to eq("Diana D")
+      expect(candidate1.party).to eq(:democrat)
+    end
+  end
 end
