783f9ec3f381a8f26f9e132d4e584808d28bff6e
KaraJH <karas.jones@yahoo.com>
2022-11-01 09:08:50 -0600

    Creates first test for the Candidate class

---
+++ spec/candidate_spec.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+require './lib/candidate'
+
+RSpec.describe Candidate do
+  it 'is an object of the Candidate class' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+
+    expect(diana).to be_a(Candidate)
+  end
+  it 'has a name and a party' do
+    diana = Candidate.new({name: "Diana D", party: :democrat})
+
+    expect(diana.name).to eq("Diana D")
+    expect(diana.party).to eq(:democrat)
+  end
+end
