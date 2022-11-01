31e98266bd15d41a2a5492a6bf9081d27f31435c
KaraJH <karas.jones@yahoo.com>
2022-11-01 10:24:39 -0600

    Stuck on writing the test for register candidate

--- spec/race_spec.rb
+++ spec/race_spec.rb
@@ -17,15 +17,20 @@ RSpec.describe Race do
       expect(race.candidates).to eq([])
     end
   end
-
-  describe '#register_candidate!' do
-    it 'takes a has of a candidate object and returns the instance of Candidate' do
-      race = Race.new("Texas Governor")
-      candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
-
-      expect(candidate1).to be_a(Candidate)
-      expect(candidate1.name).to eq("Diana D")
-      expect(candidate1.party).to eq(:democrat)
-    end
-  end
+#
+#   describe '#register_candidate!' do
+#     it 'takes a hash of a candidate object and returns the instance of Candidate' do
+#       race = Race.new("Texas Governor")
+#
+#       # require 'pry' ;binding.pry
+#       race.register_candidate!(Candidate.new({}))
+#       candidate1 = race.register_candidate!({name: "Diana D", party: :democrat})
+#       candidate1 = Candidate.new({name: "Diana D", party: :democrat})
+#
+#       expect(candidate1.class).to eq(Candidate)
+#       expect(candidate1.name).to eq("Diana D")
+#       expect(candidate1.party).to eq(:democrat)
+#       expect(race.candidates).to eq([candidate1])
+#     end
+#   end
 end
